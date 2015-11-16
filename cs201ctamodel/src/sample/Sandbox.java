dc1881ce563d64b5d14a761e0241054264ffaf00
Vinesh Kannan <vingkan@gmail.com>
2015-11-16 05:29:19 -0600

    Successfully retrieved values from KML <description> section

--- cs201ctamodel/src/sample/Sandbox.java
+++ cs201ctamodel/src/sample/Sandbox.java
@@ -1,13 +1,46 @@
 package sample;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Scanner;
 
 public class Sandbox {
 
 	public static void main(String[] args) throws Exception{
+		
+		Scanner scan = new Scanner(new File("35bronzeiit-placemark.txt"));
+		String key = "NAME";
+		boolean reading = false;
+		while(scan.hasNextLine()){
+			String nextLine = scan.nextLine();
+				if(nextLine.length() > 2){
+				if(reading){
+					System.out.println(stripTags(nextLine));
+					reading = false;
+				}
+				else if(nextLine.contains(key)){
+					//System.out.println(stripTags(nextLine));
+					reading = true;
+				}
+			}
+			//System.out.println(nextLine);
+		}
+		scan.close();
+		
+	}
+	
+	public static String stripTags(String line){
+		String value = line;
+		value = value.replace("<td>", "");
+		value = value.replace("</td>", "");
+		value = value.substring(1);
+		return value;
+	}
+	
+	public static void httpGetRequestTry() throws Exception{
 		String url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx";
 		String charset = "UTF-8";
 		String key = "topkek";
