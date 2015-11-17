f358df3ad0831830d2c6a0cbdfac7a27ccd1a80a
Vinesh Kannan <vingkan@gmail.com>
2015-11-16 23:53:15 -0600

    Model changes

--- cs201ctamodel/src/sample/Sandbox.java
+++ cs201ctamodel/src/sample/Sandbox.java
@@ -5,31 +5,54 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sandbox {
 
 	public static void main(String[] args) throws Exception{
 		
-		Scanner scan = new Scanner(new File("greenline-placemark.txt"));
-		/*String key = "NAME";
-		boolean reading = false;*/
+		Scanner scan = new Scanner(new File("full-kml.txt"));
+		String key = "DESCRIPTION";
+		boolean reading = false;
+		boolean readingPlacemark = false;
+		List<String> results = new ArrayList<String>();
 		while(scan.hasNextLine()){
 			String nextLine = scan.nextLine();
-			/*if(nextLine.length() > 2){
-				if(reading){
-					System.out.println(stripTags(nextLine));
-					reading = false;
-				}
-				else if(nextLine.contains(key)){
-					//System.out.println(stripTags(nextLine));
-					reading = true;
-				}
-			}*/
-			System.out.println(nextLine);
+			//System.out.print(nextLine);
+			if(nextLine.contains("</Placemark>")){
+				readingPlacemark = false;
+				//System.out.println("----------");
+			}
+			else if(readingPlacemark){
+				//System.out.println(nextLine);
+				reading = handlePlacemark(results, nextLine, key, reading);
+				//reading = handlePlacemark(nextLine, "RAIL LINES", reading);
+			}
+			else if(nextLine.contains("<Placemark>")){
+				readingPlacemark = true;
+				//System.out.println("----------");
+			}
 		}
 		scan.close();
-		
+		for(String result : results){
+			System.out.println(result);
+		}
+	}
+	
+	public static boolean handlePlacemark(List<String> results, String nextLine, String key, boolean reading){
+		if(nextLine.length() > 2){
+			if(reading){
+				results.add(stripTags(nextLine));
+				reading = false;
+			}
+			else if(nextLine.contains(key)){
+				//System.out.println(stripTags(nextLine));
+				reading = true;
+			}
+		}
+		return reading;
 	}
 	
 	public static String stripTags(String line){
