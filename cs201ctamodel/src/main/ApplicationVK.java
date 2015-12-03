606f3db121c49de731678a8a977060b91f189c86
Vinesh Kannan <vingkan@gmail.com>
2015-12-03 15:24:29 -0600

    Scan .csv file

--- cs201ctamodel/src/main/ApplicationVK.java
+++ cs201ctamodel/src/main/ApplicationVK.java
@@ -1,9 +1,24 @@
 package main;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class ApplicationVK {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File file = new File("cta-system-stops.csv");
+		try{
+			Scanner scan = new Scanner(file);
+			while(scan.hasNextLine()){
+				String line = scan.nextLine();
+				System.out.println(line);
+			}
+			scan.close();
+		}
+		catch(Exception e){
+			System.out.println("Encountered Exception: " + e);
+		}
 
 	}
 
