df64bad518f3ca6a047bebd71dff9b070355ed97
Vinesh Kannan <vingkan@gmail.com>
2015-12-03 15:27:47 -0600

    Added new CTAStop Class

---
+++ cs201ctamodel/src/main/CTAStop.java
@@ -0,0 +1,29 @@
+package main;
+
+public class CTAStop {
+
+	private int id;
+	private String name;
+	
+	public CTAStop(){
+		this.id = 0;
+		this.name = "Blank Stop";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
