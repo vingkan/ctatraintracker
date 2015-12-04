5d907d061849966378b633feb7d362352a97d97d
Vinesh Kannan <vingkan@gmail.com>
2015-12-03 19:40:28 -0600

    We will have stops inside stations

---
+++ cs201ctamodel/src/main/CTAStation.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CTAStation {
+
+	private int id;
+	private String name;
+	private List<CTAStop> stops;
+	
+	public CTAStation(int id, String name){
+		this.id = id;
+		this.name = name;
+		this.stops = new ArrayList<CTAStop>();
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public void setID(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<CTAStop> getStops() {
+		return stops;
+	}
+
+	public void setStops(List<CTAStop> stops) {
+		this.stops = stops;
+	}
+	
+}
--- cs201ctamodel/src/main/CTAStop.java
+++ cs201ctamodel/src/main/CTAStop.java
@@ -4,18 +4,22 @@ public class CTAStop extends Location{
 
 	private int id;
 	private String name;
+	private int stationID;
+	private int routeID;
+	
 	
 	public CTAStop(){
 		super();
-		this.id = 0;
+		this.id = -1;
 		this.name = "Blank Stop";
+		this.stationID = -1;
 	}
 
-	public int getId() {
+	public int getID() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setID(int id) {
 		this.id = id;
 	}
 
@@ -25,6 +29,22 @@ public class CTAStop extends Location{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getStationID() {
+		return stationID;
+	}
+
+	public void setStationID(int stationID) {
+		this.stationID = stationID;
+	}
+
+	public int getRouteID() {
+		return routeID;
+	}
+
+	public void setRouteID(int routeID) {
+		this.routeID = routeID;
 	}
 	
 }
