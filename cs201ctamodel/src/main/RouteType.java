c05ee408047b24ad2eb92b5e1e3411c6dbbb92dc
Vinesh Kannan <vingkan@gmail.com>
2015-12-04 01:12:12 -0600

    Custom green color from cta map

--- cs201ctamodel/src/main/RouteType.java
+++ cs201ctamodel/src/main/RouteType.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public enum RouteType {
 	
 	CUSTOM ("Custom Route", Color.GRAY),
-	GREEN_LINE ("Green Line", Color.GREEN),
+	GREEN_LINE ("Green Line", new Color(0x00A950)),
 	RED_LINE ("Red Line", Color.RED),
 	BLUE_LINE ("Blue Line", Color.BLUE),
 	ORANGE_LINE ("Orange Line", Color.ORANGE),
